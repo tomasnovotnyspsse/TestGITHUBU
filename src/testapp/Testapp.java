722aecfe4d16817c4a8ae68cc13be559aa2d3f4a
Tom <Tom@10.2.110.119>
2016-05-11 09:16:09 +0200

    UVODNI COMMIT

---
+++ src/testapp/Testapp.java
@@ -0,0 +1,13 @@
+package testapp;
+
+
+public class Testapp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.err.println("AHOJKY");
+    }
+    
+}
